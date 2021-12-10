7fe1e992b284b03e4a61bfdd204c4c57cefc2686
thednp <dnp.digital@gmail.com>
2021-12-10 08:53:49 +0200

    1

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,8 +2,3 @@ export as namespace CubicBezier;
 export default CubicBezier;
 
 import {default as CubicBezier} from "cubic-bezier-easing/src/cubic-bezier-easing";
-
-// alias
-declare module "cubic-bezier-easing/dist/cubic-bezier-easing.esm" {
-  export { default as CubicBezier } from "cubic-bezier-easing/src/cubic-bezier-easing";
-}
